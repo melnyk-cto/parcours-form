e3f47e2dea949569889788b89180d1df67218c75
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-01-15 02:23:40 +0200

    features: back

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     const question = document.getElementsByClassName('question');
+    const prevBtn = document.getElementById('prev-btn');
     const nextBtn = document.getElementById('next-btn');
     let count = 0;
 
@@ -10,6 +11,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // Show Next Step  on click button
     nextBtn.addEventListener('click', function () {
         if (count < 25) count += 1;
+
+        for (let i = 0; i < question.length; i++) {
+            question[i].setAttribute('style', 'display:none !important');
+        }
+
+        question[count].setAttribute('style', 'display:block !important');
+
+    });
+
+    // Show Prev Step  on click button
+    prevBtn.addEventListener('click', function () {
+        if (count > 0) count -= 1;
 
         for (let i = 0; i < question.length; i++) {
             question[i].setAttribute('style', 'display:none !important');
