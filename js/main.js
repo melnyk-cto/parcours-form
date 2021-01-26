4211c8fe6e32337bad89f1da6b52c36a9254626f
Mykola Melnyk <melnyk.cto.dev@gmail.com>
2021-01-26 13:16:34 +0200

    features: 3 steps done

--- js/main.js
+++ js/main.js
@@ -7,55 +7,92 @@ document.addEventListener('DOMContentLoaded', function () {
   const continueBtn = document.getElementsByClassName("continue")[0];
   const progressLine = document.getElementById("progress");
   const input = document.getElementsByTagName("input");
+  const goFirstStep = document.getElementsByClassName("go-first-step")[0];
   let count = 0;
+  let historySteps = [];
 
-  function nextStep() {
-    if (count < question.length - 1) count += 1;
+  const goToStep = (current) => {
+    count = current;
+
+    // Hide all steps
     for (let i = 0; i < question.length; i++) {
       question[i].setAttribute('style', 'display:none !important');
     }
+
+    // Current Step
     question[count].setAttribute('style', 'display:block !important');
 
-    if (count === question.length - 1) {
-      progress.setAttribute('style', 'display:none !important');
-      continueBtn.setAttribute('style', 'display:none !important');
-    } else {
-      progress.setAttribute('style', 'display:block !important');
-      continueBtn.setAttribute('style', 'display:block !important');
-    }
     progressLine.style.width = `${count / question.length * 100}%`;
 
-  }
+  };
 
   // Show  Step 1 on load
   question[count].setAttribute('style', 'display:block !important');
 
   // Show Next Step  on click button
   nextBtn.addEventListener('click', function () {
-    nextStep();
+    if (count < question.length - 1) count += 1;
+
+
+  });
+  // Go to First Step  on click button
+  goFirstStep.addEventListener('click', function () {
+    goToStep(0);
+    historySteps.length = 0;
+
   });
 
   // Show Prev Step  on click button
   prevBtn.addEventListener('click', function () {
-    if (count > 0) count -= 1;
-
-    for (let i = 0; i < question.length; i++) {
-      question[i].setAttribute('style', 'display:none !important');
+    if (historySteps.length > 0) {
+      if (historySteps.length > 1) {
+        count = historySteps[historySteps.length - 2];
+        goToStep(count);
+        historySteps.pop();
+      } else {
+        count = historySteps[historySteps.length - 1] - 1;
+        goToStep(count);
+      }
     }
-
-    question[count].setAttribute('style', 'display:block !important');
-
   });
 
 
   // Go to next when click on input
   for (let i = 0; i < input.length; i++) {
     input[i].addEventListener('click', function () {
+      if (count < question.length - 1) count += 1;
+
       const inputRadio = input[i].getAttribute('type');
       if (inputRadio === 'radio') {
-        setTimeout(()=> {
-          nextStep();
-        }, 400);
+        setTimeout(() => {
+          // Show current step
+          switch (count) {
+            case 2:
+              if (this.getAttribute('data-name') === 'yes') {
+                count += 1;
+                goToStep(count);
+              } else {
+                goToStep(count);
+              }
+              break;
+            case 5:
+              break;
+            default:
+              goToStep(count);
+          }
+
+          //  Hide progress bar adn button s on last step
+          if (count === question.length - 1) {
+            progress.setAttribute('style', 'display:none !important');
+            continueBtn.setAttribute('style', 'display:none !important');
+          } else {
+            progress.setAttribute('style', 'display:block !important');
+            continueBtn.setAttribute('style', 'display:block !important');
+          }
+
+          historySteps.push(count);
+
+        }, 200);
       }
     });
 
